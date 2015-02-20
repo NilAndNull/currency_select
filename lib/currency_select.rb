ea21da1c7171de87e1a53dc8f3c7e751d7967efe
João <sieroaoj@gmail.com>
2015-02-20 22:20:18 +0000

    filter_currencies paramenter
    We needed to remove some currencies from the select so I created a filter argument for that.
    Just pass an array like: filter_currencies = ['btc'] and the select will not have this currency

--- lib/currency_select.rb
+++ lib/currency_select.rb
@@ -5,14 +5,17 @@ module CurrencySelect
 
     # Returns an array with ISO codes and currency names for <tt>option</tt>
     # tags.
-    def currencies_array
+    def currencies_array(filter_currencies)
       if @currency_list
         @currency_list
       else
         @currencies ||= begin
           require 'money'
           Money::Currency::table.inject([]) do |array, (id, currency)|
-            array << [ "#{currency[:name]} - #{currency[:iso_code]}", id ]
+            if filter_currencies.nil? || (filter_currencies.include?(currency[:iso_code]) == false)
+              array << [ "#{currency[:name]} - #{currency[:iso_code]}", id ]
+            end
+            array
           end.sort_by { |currency| currency.first }
         end
       end
@@ -23,8 +26,8 @@ module CurrencySelect
     # == Example
     #   priority_currencies_array([ "USD", "NOK" ])
     #   # => [ ['United States Dollar - USD', 'USD' ], ['Norwegian Kroner - NOK', 'NOK'] ]
-    def priority_currencies_array(currency_codes = [])
-      currency_codes.flat_map { |code| currencies_array.select { |currency| currency.last.to_s == code }}
+    def priority_currencies_array(filter_currencies,currency_codes = [])
+      currency_codes.flat_map { |code| currencies_array(filter_currencies).select { |currency| currency.last.to_s == code }}
     end
 
   end
@@ -37,7 +40,7 @@ module ActionView
 
       # Return select and option tags for the given object and method, using
       # currency_options_for_select to generate the list of option tags.
-      def currency_select(object, method, priority_currencies = nil, options = {}, html_options = {})
+      def currency_select(object, method, filter_currencies = nil, priority_currencies = nil, options = {}, html_options = {})
         tag = if defined?(ActionView::Helpers::InstanceTag) &&
                 ActionView::Helpers::InstanceTag.instance_method(:initialize).arity != 0
 
@@ -45,8 +48,7 @@ module ActionView
               else
                 CurrencySelectTag.new(object, method, self, options)
               end
-
-        tag.to_currency_select_tag(priority_currencies, options, html_options)
+        tag.to_currency_select_tag(filter_currencies, priority_currencies, options, html_options)
       end
 
       # Returns a string of option tags for all available currencies. Supply
@@ -54,11 +56,11 @@ module ActionView
       # option tag. You can also supply an array of currencies as
       # +priority_currencies+, so that they will be listed above the rest of
       # the list.
-      def currency_options_for_select(selected = nil, priority_currencies = nil)
+      def currency_options_for_select(selected = nil, filter_currencies = nil, priority_currencies = nil)
         currency_options = "".html_safe
 
         if priority_currencies
-          currency_options += options_for_select(::CurrencySelect::priority_currencies_array(priority_currencies), selected)
+          currency_options += options_for_select(::CurrencySelect::priority_currencies_array(filter_currencies,priority_currencies), selected)
           currency_options += "<option value=\"\" disabled=\"disabled\">-------------</option>\n".html_safe
 
           # prevents selected from being included twice in the HTML which causes
@@ -67,17 +69,16 @@ module ActionView
           selected = nil if priority_currencies.include?(selected)
         end
 
-        # All the countries included in the country_options output.
-        return currency_options + options_for_select(::CurrencySelect::currencies_array, selected)
+        return currency_options + options_for_select(::CurrencySelect::currencies_array(filter_currencies), selected)
       end
     end
 
     module ToCurrencySelectTag
-      def to_currency_select_tag(priority_currencies, options, html_options)
+      def to_currency_select_tag(filter_currencies, priority_currencies, options, html_options)
         html_options = html_options.stringify_keys
         add_default_name_and_id(html_options)
         value = value(object)
-        content_tag('select', add_options(currency_options_for_select(value, priority_currencies), options, value), html_options)
+        content_tag('select', add_options(currency_options_for_select(value, filter_currencies, priority_currencies), options, value), html_options)
       end
     end
 
@@ -93,8 +94,8 @@ module ActionView
     end
 
     class FormBuilder
-      def currency_select(method, priority_currencies = nil, options = {}, html_options = {})
-        @template.currency_select(@object_name, method, priority_currencies, options.merge(:object => @object), html_options)
+      def currency_select(method, filter_currencies = nil, priority_currencies = nil, options = {}, html_options = {})
+        @template.currency_select(@object_name, method, filter_currencies, priority_currencies, options.merge(:object => @object), html_options)
       end
     end
 
